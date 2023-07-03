5fa3b76c46e36fb9fb106df122aa0d8ab6555c13
duchieu2122003 <duchieunguyen2122003@gmail.com>
2023-07-03 23:50:03 +0700

    Commit add session 1.5 Java 6

---
+++ self_study_java6/src/main/java/com/example/self_study_java6/Main.java
@@ -0,0 +1,8 @@
+package com.example.self_study_java6;
+
+/**
+ * By Duc Hiiu212
+ */
+public class Main {
+    
+}
---
+++ self_study_java6/src/main/java/com/example/self_study_java6/SelfStudyJava6Application.java
@@ -0,0 +1,13 @@
+package com.example.self_study_java6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfStudyJava6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfStudyJava6Application.class, args);
+    }
+
+}
