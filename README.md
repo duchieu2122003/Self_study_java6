60557c94ea2a8c41527aaed1c69ad8bacd89fdb1
duchieu2122003 <duchieunguyen2122003@gmail.com>
2024-04-05 16:38:59 +0700

    setup pro

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Self_study_java6
+
+# slides về RESTFULL API kết hợp với Thymeleaf
--- self_study_java6/src/main/java/com/example/self_study_java6/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example.self_study_java6;
-
-/**
- * By Duc Hiiu212
- */
-public class Main {
-    
-}
